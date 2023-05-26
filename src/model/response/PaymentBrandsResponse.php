b88a0d0453d8fdd71ebfdc800fd52e8fd3d52147
Casper Langemeijer <casper.langemeijer@procurios.nl>
2023-05-26 15:07:43 +0200

    Fix method signatures to be compatible with PHP 8.1's JsonSerializable::jsonSerialize()

--- src/model/response/PaymentBrandsResponse.php
+++ src/model/response/PaymentBrandsResponse.php
@@ -46,10 +46,7 @@ class PaymentBrandsResponse implements JsonSerializable
         return $this->paymentBrands;
     }
 
-    /**
-     * @return array
-     */
-    public function jsonSerialize()
+    public function jsonSerialize(): array
     {
         $json = [];
         foreach ($this as $key => $value) {
